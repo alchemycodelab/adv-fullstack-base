f89a968966a69663e5be97946c822ee904b540b2
Logan Barnett <logan@alchemycodelab.com>
2022-11-07 11:28:26 -0800

    do not use global.css yet

--- client/app.tsx
+++ client/app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './reset.css';
-import './global.css';
+// import './global.css';
 import fooListFn from './components/foo-list'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
