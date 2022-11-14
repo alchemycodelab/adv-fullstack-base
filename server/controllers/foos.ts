cf4e7bb8989d03b3e794d3d4bf5e2d67c39942c5
Logan Barnett <logan@alchemycodelab.com>
2022-11-13 23:01:52 -0800

    add interactions to client tests

--- server/controllers/foos.ts
+++ server/controllers/foos.ts
@@ -9,3 +9,14 @@ export default Router()
     return db.query('select * from foos;')
       .then((result: QueryResult<Foo>) => res.send(result.rows))
   })
+  .delete('/:id', (req: Request, res: Response) => {
+    if(req.params.id != null && Number.isInteger(parseInt(req.params.id))) {
+      return db.query('delete from foos where foos.id = ?', [req.params.id])
+        .then(() => res.status(201))
+    } else {
+      console.error(
+        `id to delete is not a valid id: ${JSON.stringify(req.params.id)}`,
+      )
+      res.status(400)
+    }
+  })
