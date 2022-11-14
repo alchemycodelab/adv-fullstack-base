16891691728c39b33a6a6077894d9b64d265aa04
Logan Barnett <logan@alchemycodelab.com>
2022-11-13 23:01:52 -0800

    add interactions to client tests

--- client/components/foo-list.test.tsx
+++ client/components/foo-list.test.tsx
@@ -1,16 +1,15 @@
-import { act, render, screen, waitFor } from '@testing-library/react';
+import {
+  act,
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+} from '@testing-library/react';
 import fooListFn from './foo-list'
 import {afterEach, expect, jest, describe, it} from '@jest/globals'
-
-// TODO: Make mocking sane.
-global.fetch = jest.fn((..._args: any) => {
-  return (Promise.resolve({
-    status: 200,
-    json: (): any => {
-      return Promise.resolve([{foo: 'bar'}])
-    },
-  }) as Promise<Response>)
-})
+import '@testing-library/jest-dom'
+import '@testing-library/jest-dom/extend-expect'
+import { mockFetch } from '../mock-fetch'
 
 describe('FooList', () => {
   // Can we set this globally for all tests? That would be the swellest.
@@ -19,6 +18,7 @@ describe('FooList', () => {
   })
 
   it('displays a list of foos from a request', async () => {
+    mockFetch(200, [{id: 0, foo: 'bar'}])
     const FooList = fooListFn()
     // When testing useEffect and other async operations, use act + waitFor like
     // in this test.
@@ -27,7 +27,6 @@ describe('FooList', () => {
         <FooList />
       )
     })
-
     // We must wait for the results to come back. Because we have this
     // distinction, we can test loading UI (sans waitFor) and results (with
     // waitFor).
@@ -36,4 +35,46 @@ describe('FooList', () => {
       expect(el.textContent).toEqual('bar')
     })
   })
+
+  it('deletes a foo and removes it from display', async () => {
+    const FooList = fooListFn()
+    // When testing useEffect and other async operations, use act + waitFor like
+    // in this test.
+    await act(async () => {
+      render(
+        <FooList />
+      )
+    })
+    await act(() => {
+      mockFetch(200, [])
+      const deleteButton = screen.getByText('delete')
+      fireEvent(
+        deleteButton,
+        new MouseEvent('click', {
+          bubbles: true,
+          cancelable: true,
+        }),
+      )
+    })
+    // We must wait for the results to come back. Because we have this
+    // distinction, we can test loading UI (sans waitFor) and results (with
+    // waitFor).
+    await waitFor(async () => {
+      // The toBeInTheDocument matcher doesn't work with TypeScript, even though
+      // it is documented here:
+      // https://testing-library.com/docs/guide-disappearance/#nottobeinthedocument
+      //
+      // Bugs are reported in lots of places, but one such as this can be found
+      // about it: https://github.com/testing-library/jest-dom/issues/442
+      //
+      // This seems to be something that jest-dom and friends are somewhat aware
+      // of but even with minimal reproductions, the problem doesn't seem to be
+      // well understood. There's also the possibility of version-mismatches
+      // between the type definitions and the library itself. Node.js project
+      // configurations are so incredibly bespoke that it's hard for them to
+      // narrow it down.
+      expect(screen.queryByTestId('foo-0')).toBeNull()
+    })
+  })
+
 })
--- client/components/foo-list.tsx
+++ client/components/foo-list.tsx
@@ -4,6 +4,9 @@ import {
   useState,
   useEffect,
 } from 'react'
+import { deleteFoo, getFoos } from '../services/foos'
+import buttonFn from './button'
+import buttonStyles from './button.module.css'
 import listItemFn from './list-item'
 import listItemStyles from './list-item.module.css'
 import { type Foo } from '../../common/foo.js'
@@ -14,27 +17,40 @@ export type Component = FC<Props>
 
 export default (): FC<Props> => {
   const ListItem = listItemFn(listItemStyles.foo)
+  const DeleteButton = buttonFn(buttonStyles.removeListItem)
   const component = (props: Props): ReactElement => {
     // Simply accepting the type we get from the server is inherently dangerous,
     // but for the simplicity of the example we will forego validation.
     const [foos, setFoos ] = useState<ReadonlyArray<Foo>>([])
+    const [loadingFoos, setLoadingFoos] = useState(false)
     const [error, setError ] = useState<{} | null>(null)
+    const loadFoos = () => {
+      setLoadingFoos(true)
+      getFoos()
+        .then(res => (res.status < 400 ? setFoos : setError)(res.json))
+        .finally(() => setLoadingFoos(false))
+    }
     useEffect(() => {
-      fetch('/api/v1/foos').then((res) => {
-        return res.json().then(res.status < 400 ? setFoos : setError)
-      })
+      loadFoos()
     }, [])
-    console.log('foos', foos)
     if(foos.length > 0) {
       return <ul>
-        {foos.map((foo, i) => {
-          return <ListItem key={i}>
-            <span data-testid={'foo-' + i}>{foo.foo}</span>
+        {foos.map((foo) => {
+          return <ListItem key={foo.id}>
+            <span data-testid={'foo-' + foo.id}>{foo.foo}</span>
+            <DeleteButton
+              dataTestId={'foo-delete-' + foo.id}
+              onClick={() => deleteFoo(foo.id).then(loadFoos)}
+            >
+              delete
+            </DeleteButton>
           </ListItem>
         })}
       </ul>
     } else if(error != null) {
       return <span style={{color: 'red'}}>{JSON.stringify(error)}</span>
+    } else if(!loadingFoos) {
+      return <span>No Foos found!</span>
     } else {
       return <span>Loading Foos!</span>
     }
